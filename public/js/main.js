62f596bbf6d1bba7ddbba26e6d4e5370814fd115
CoolMcProgrammer123 <86947265+CoolMcProgrammer123@users.noreply.github.com>
2025-03-31 17:53:22 -0400

    Commented out old code

--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,18 @@
+/*
+
 console.log("Hello World");
 
-// let test = '{"name": "abc","id": 21,"class": "S2123"}'
+let test = '{"name": "abc","id": 21,"class": "S2123"}'
 
-// let result = JSON.parse(test)
+let result = JSON.parse(test)
 
-// console.log("String version of test: ", test);
-// console.log("JSON version of test: ", result);
+console.log("String version of test: ", test);
+console.log("JSON version of test: ", result);
 
 let newTest = {"name": "abc","id": 21,"class": "S2123"};
 console.log("JSON version of newTest: ", newTest);
 
 let resultString = JSON.stringify(newTest);
-console.log("String version of newTest: ", resultString);
+console.log("String version of newTest: ", resultString); 
+
+*/
