bb25a35ffce6e38ea5a8b780ab090759ea58f7ea
CoolMcProgrammer123 <86947265+CoolMcProgrammer123@users.noreply.github.com>
2025-03-31 17:52:22 -0400

    Set up document

---
+++ public/js/main.js
@@ -0,0 +1,14 @@
+console.log("Hello World");
+
+// let test = '{"name": "abc","id": 21,"class": "S2123"}'
+
+// let result = JSON.parse(test)
+
+// console.log("String version of test: ", test);
+// console.log("JSON version of test: ", result);
+
+let newTest = {"name": "abc","id": 21,"class": "S2123"};
+console.log("JSON version of newTest: ", newTest);
+
+let resultString = JSON.stringify(newTest);
+console.log("String version of newTest: ", resultString);
